278a3f932769dcdc6ad36623b031d2eb5cd61755
RafaelFigueiredo2203 <rafaelfigueiedojunior7@gmail.com>
2023-11-28 17:37:27 -0300

    Finalizando botão de cadastrar nova transação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import logo from '../../assets/logo.svg';
+import { NewTransactionModal } from '../NewTransactionModal';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 
@@ -8,7 +10,13 @@ export function Header(){
       <HeaderContent>
         <img src={logo} alt="" />
 
+      <Dialog.Root>
+      <Dialog.Trigger asChild>
       <NewTransactionButton>Nova Transação </NewTransactionButton>
+      </Dialog.Trigger>
+
+      <NewTransactionModal/>
+      </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
