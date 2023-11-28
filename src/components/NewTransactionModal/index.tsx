a4a8ddfda70a41906138f9e95f66c291b61aea53
RafaelFigueiredo2203 <rafaelfigueiedojunior7@gmail.com>
2023-11-28 18:05:32 -0300

    Configurando Axios

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as Dialog from "@radix-ui/react-dialog";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import * as z from 'zod';
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
 
@@ -9,15 +9,18 @@ const newTransactionFormSchema = z.object({
   description:z.string(),
   price:z.number(),
   category: z.string(),
-  //type: z.enum(['income','outcome']),
+  type: z.enum(['income','outcome']),
 })
 
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal(){
 
-  const {register, handleSubmit , formState:{isSubmitting}} = useForm<NewTransactionFormInputs>({
-    resolver: zodResolver(newTransactionFormSchema)
+  const {control, register, handleSubmit , formState:{isSubmitting}} = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+    defaultValues:{
+      type:'income'
+    }
   })
 
 async function handleCreateNewTransaction(data: NewTransactionFormInputs){
@@ -42,7 +45,15 @@ async function handleCreateNewTransaction(data: NewTransactionFormInputs){
             <input type="text" placeholder="Categoria" {...register('category')} required/>
 
 
-            <TransactionType>
+           <Controller
+           control={control}
+           name="type"
+           render={({field}) => {
+            return(
+              <TransactionType
+              onValueChange={field.onChange}
+              value={field.value}
+              >
               <TransactionTypeButton value="income" variant='income'>
                 <ArrowCircleUp size={24}/>
                 Entrada
@@ -53,6 +64,9 @@ async function handleCreateNewTransaction(data: NewTransactionFormInputs){
                 Saída
               </TransactionTypeButton>
             </TransactionType>
+            )
+           }}
+           />
 
 
             <button type="submit" disabled={isSubmitting}>Cadastrar</button>
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,7 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { MagnifyingGlass } from "phosphor-react";
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import * as z from 'zod';
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 import { SearchFormContainer } from "./styles";
 
 const searchFormSchema = z.object({
@@ -13,6 +15,8 @@ type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm(){
 
+  const {fetchTransactions} = useContext(TransactionsContext);
+
   const {register, handleSubmit, formState:{
     isSubmitting
   }} = useForm<SearchFormInputs>({
@@ -20,9 +24,7 @@ export function SearchForm(){
   });
 
   async function handleSearchTransactins(data: SearchFormInputs){
-    await new Promise(resolve => setTimeout(resolve, 2000))
-
-    console.log(data);
+    await fetchTransactions(data.query)
   }
 
 
